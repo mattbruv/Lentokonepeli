a39fea45021ddd309145231ac336965c360dd582
Matt <5638426+mattbruv@users.noreply.github.com>
2019-08-07 16:48:14 -0400

    stopping point

--- client/src/render/sprites/ground.ts
+++ client/src/render/sprites/ground.ts
@@ -1,3 +1,4 @@
+import * as PIXI from "pixi.js";
 import { GameSprite } from "./gameSprite";
 import { Ground } from "../../../../dogfight/src/entities/ground";
 import { spriteSheet } from "../textures";
@@ -14,18 +15,24 @@ export class GroundSprite implements GameSprite<Ground> {
   public constructor(world: PIXI.Container) {
     this.container = world;
     this.groundContainer = new PIXI.Container();
+    // Initialize sprites
     this.groundLeft = new PIXI.Sprite(spriteSheet.textures["src/beach-l.gif"]);
     this.groundMid = new PIXI.Sprite(spriteSheet.textures["src/ground1.gif"]);
     this.groundRight = new PIXI.Sprite(spriteSheet.textures["src/beach-l.gif"]);
+
+    // Set sprite anchors
+    this.groundMid.anchor.set(0.5);
   }
 
   public addSprite(data: Ground): void {
+    this.groundContainer.addChild(this.groundLeft);
+    this.groundContainer.addChild(this.groundMid);
+    this.groundContainer.addChild(this.groundRight);
+    this.container.addChild(this.groundContainer);
     console.log("create" + data);
   }
 
-  public updateSprite(data: Ground): void {
-    console.log("update" + data);
-  }
+  public updateSprite(data: Ground): void {}
 
   public deleteSprite(): void {
     console.log("remove self");
--- client/world/main.ts
+++ client/world/main.ts
@@ -1,6 +1,7 @@
 import * as PIXI from "pixi.js";
 import { newGameWorld } from "../../dogfight/src/engine/world";
 import { loadSpriteSheet, spriteSheet } from "../src/render/textures";
+import { RenderWorld, renderNewWorldState } from "../src/render/worldRenderer";
 
 const app = new PIXI.Application({
   width: 750,
@@ -11,7 +12,18 @@ const app = new PIXI.Application({
 
 const world = newGameWorld();
 
+world.grounds.push({
+  position: { x: 30, y: 30 },
+  width: 500
+});
+
+const renderer: RenderWorld = {
+  world: app.stage,
+  grounds: []
+};
+
 function init(): void {
+  renderNewWorldState(renderer, world);
   console.log(spriteSheet);
   console.log(world);
   document.body.appendChild(app.view);
