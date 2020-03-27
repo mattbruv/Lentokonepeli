67d2e62c59294c5e8f959f4bf01a4a5cb7ef68c7
Matt <5638426+mattbruv@users.noreply.github.com>
2020-03-26 23:30:46 -0400

    add plane object

--- dogfight/src/objects/plane.ts
+++ dogfight/src/objects/plane.ts
@@ -1,3 +1,7 @@
+import { GameObjectType, GameObject } from "../object";
+import { Team, SCALE_FACTOR } from "../constants";
+import { Cache, CacheEntry } from "../network/cache";
+
 export enum PlaneType {
   Albatros,
   Junkers,
@@ -5,4 +9,60 @@ export enum PlaneType {
   Bristol,
   Salmson,
   Sopwith
+}
+
+export class Plane extends GameObject {
+  public type = GameObjectType.Plane;
+
+  public localX: number;
+  public localY: number;
+  public x: number;
+  public y: number;
+
+  public planeType: PlaneType;
+  public team: Team;
+  public health: number;
+  public direction: number;
+  public flipped: boolean;
+
+  public constructor(id: number, cache: Cache, kind: PlaneType, side: Team) {
+    super(id);
+    this.localX = 0;
+    this.localY = 0;
+    this.setData(cache, {
+      x: 0,
+      y: 0,
+      flipped: false,
+      direction: 0,
+      health: 100,
+      planeType: kind,
+      team: side
+    });
+  }
+
+  public setPos(cache: Cache, x: number, y: number): void {
+    this.localX = x * SCALE_FACTOR;
+    this.localY = y * SCALE_FACTOR;
+    this.setData(cache, { x, y });
+  }
+
+  public move(cache: Cache, deltaTime: number): void {
+    const unitsPerSecond = 100 * SCALE_FACTOR;
+    const multiplier = deltaTime / 1000;
+    this.localX = this.localX + Math.round(multiplier * unitsPerSecond);
+    this.set(cache, "x", Math.round(this.localX / SCALE_FACTOR));
+  }
+
+  public getState(): CacheEntry {
+    return {
+      type: this.type,
+      x: this.x,
+      y: this.y,
+      flipped: this.flipped,
+      direction: this.direction,
+      health: this.health,
+      planeType: this.planeType,
+      team: this.team
+    };
+  }
 }
--- dogfight/src/world.ts
+++ dogfight/src/world.ts
@@ -61,6 +61,7 @@ export class GameWorld {
    * @param timestep Number of milliseconds to advance simulation
    */
   public tick(deltaTime: number): Cache {
+    /*
     if (this.troopers.length == 0) {
       const man = new Trooper(this.nextID(), this.cache);
       man.setPos(this.cache, 0, 100);
@@ -74,6 +75,7 @@ export class GameWorld {
         this.removeObject(this.troopers, trooper);
       }
     }
+    */
     return this.cache;
   }
 
