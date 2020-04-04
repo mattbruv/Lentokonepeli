51fcff4a91a33f13905c5c2a36713380d212230a
Matt <5638426+mattbruv@users.noreply.github.com>
2020-04-03 20:23:49 -0400

    add object schema types

--- dogfight/src/network/schemas.ts
+++ dogfight/src/network/schemas.ts
@@ -1,7 +1,7 @@
 import { IntType, GameObjectSchema } from "./types";
 import { GameObjectType } from "../object";
 
-export const SchemaPlane: GameObjectSchema = {
+const planeSchema: GameObjectSchema = {
   numbers: [
     { name: "x", intType: IntType.Int16 },
     { name: "y", intType: IntType.Int16 },
@@ -17,6 +17,103 @@ export const SchemaPlane: GameObjectSchema = {
   strings: []
 };
 
+const flagSchema: GameObjectSchema = {
+  numbers: [
+    { name: "x", intType: IntType.Int16 },
+    { name: "y", intType: IntType.Int16 },
+    { name: "team", intType: IntType.Uint8 }
+  ],
+  booleans: [],
+  strings: []
+};
+
+const hillSchema: GameObjectSchema = {
+  numbers: [
+    { name: "x", intType: IntType.Int16 },
+    { name: "y", intType: IntType.Int16 },
+    { name: "terrain", intType: IntType.Uint8 }
+  ],
+  booleans: [],
+  strings: []
+};
+
+const groundSchema: GameObjectSchema = {
+  numbers: [
+    { name: "x", intType: IntType.Int16 },
+    { name: "y", intType: IntType.Int16 },
+    { name: "width", intType: IntType.Uint16 },
+    { name: "terrain", intType: IntType.Uint8 }
+  ],
+  booleans: [],
+  strings: []
+};
+
+const playerSchema: GameObjectSchema = {
+  numbers: [
+    { name: "team", intType: IntType.Uint8 },
+    { name: "controlType", intType: IntType.Uint8 },
+    { name: "controlID", intType: IntType.Uint16 },
+    { name: "status", intType: IntType.Uint8 }
+  ],
+  booleans: [],
+  strings: []
+};
+
+const runwaySchema: GameObjectSchema = {
+  numbers: [
+    { name: "x", intType: IntType.Int16 },
+    { name: "y", intType: IntType.Int16 },
+    { name: "direction", intType: IntType.Uint8 },
+    { name: "team", intType: IntType.Uint8 },
+    { name: "health", intType: IntType.Uint8 }
+  ],
+  booleans: [],
+  strings: []
+};
+
+const towerSchema: GameObjectSchema = {
+  numbers: [
+    { name: "x", intType: IntType.Int16 },
+    { name: "y", intType: IntType.Int16 },
+    { name: "direction", intType: IntType.Uint8 },
+    { name: "terrain", intType: IntType.Uint8 }
+  ],
+  booleans: [],
+  strings: []
+};
+
+const trooperSchema: GameObjectSchema = {
+  numbers: [
+    { name: "x", intType: IntType.Int16 },
+    { name: "y", intType: IntType.Int16 },
+    { name: "health", intType: IntType.Uint8 },
+    { name: "state", intType: IntType.Uint8 },
+    { name: "direction", intType: IntType.Uint8 },
+    { name: "team", intType: IntType.Uint8 }
+  ],
+  booleans: [],
+  strings: []
+};
+
+const waterSchema: GameObjectSchema = {
+  numbers: [
+    { name: "x", intType: IntType.Int16 },
+    { name: "y", intType: IntType.Int16 },
+    { name: "width", intType: IntType.Uint16 },
+    { name: "direction", intType: IntType.Uint8 }
+  ],
+  booleans: [],
+  strings: []
+};
+
 export const schemaTypes = {
-  [GameObjectType.Plane]: SchemaPlane
+  [GameObjectType.Plane]: planeSchema,
+  [GameObjectType.Ground]: groundSchema,
+  [GameObjectType.Hill]: hillSchema,
+  [GameObjectType.Flag]: flagSchema,
+  [GameObjectType.Runway]: runwaySchema,
+  [GameObjectType.ControlTower]: towerSchema,
+  [GameObjectType.Trooper]: trooperSchema,
+  [GameObjectType.Player]: playerSchema,
+  [GameObjectType.Water]: waterSchema
 };
