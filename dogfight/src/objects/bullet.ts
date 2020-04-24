b57906594eb809f3bcdc871bf6409089bc7ab9e5
Matt <5638426+mattbruv@users.noreply.github.com>
2020-04-24 12:11:09 -0400

    save progress

--- dogfight/src/objects/bullet.ts
+++ dogfight/src/objects/bullet.ts
@@ -1,6 +1,6 @@
 import { GameObject, GameObjectType } from "../object";
 import { Cache, CacheEntry } from "../network/cache";
-import { Team } from "../constants";
+import { Team, SCALE_FACTOR } from "../constants";
 
 export const bulletGlobals = {
   speed: 500
@@ -9,6 +9,8 @@ export const bulletGlobals = {
 export class Bullet extends GameObject {
   public type = GameObjectType.Bullet;
   public age: number;
+  public localX: number;
+  public localY: number;
   public x: number;
   public y: number;
   public shotBy: number; // ID of player who shot it
@@ -17,12 +19,20 @@ export class Bullet extends GameObject {
 
   public constructor(id: number, cache: Cache) {
     super(id);
+    this.localX = 0;
+    this.localY = 0;
     this.setData(cache, {
       age: 0,
       x: 0,
       y: 0,
       speed: bulletGlobals.speed
     });
+  }
+
+  public setPos(cache: Cache, x: number, y: number): void {
+    this.localX = x * SCALE_FACTOR;
+    this.localY = y * SCALE_FACTOR;
+    this.setData(cache, { x, y });
   }
 
   public getState(): CacheEntry {
