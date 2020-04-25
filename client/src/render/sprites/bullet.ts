e8dfb483ea610424323af927468cbd169995361e
Matt <5638426+mattbruv@users.noreply.github.com>
2020-04-24 20:13:21 -0400

    added bullet age color fading

--- client/src/render/sprites/bullet.ts
+++ client/src/render/sprites/bullet.ts
@@ -1,6 +1,9 @@
 import * as PIXI from "pixi.js";
 import { GameSprite } from "../sprite";
 import { DrawLayer } from "../constants";
+import { bulletGlobals } from "../../../../dogfight/src/objects/bullet";
+
+const bulletColors = [0, 1118481, 3355443, 6316128, 10066329];
 
 export class BulletSprite extends GameSprite {
   public x: number;
@@ -11,6 +14,11 @@ export class BulletSprite extends GameSprite {
   private container: PIXI.Container;
 
   private bullet: PIXI.Graphics;
+
+  private timeout: number;
+
+  private bulletColorIndex: number = 0;
+  private ageRate = Math.round(bulletGlobals.lifetime / 5);
 
   public constructor(spritesheet: PIXI.Spritesheet) {
     super();
@@ -27,17 +35,32 @@ export class BulletSprite extends GameSprite {
     this.container.addChild(this.bullet);
 
     this.renderables.push(this.container);
+
+    this.timeout = window.setTimeout((): void => {
+      this.ageColor();
+    }, this.ageRate);
+  }
+
+  private ageColor(): void {
+    this.bulletColorIndex++;
+    if (this.bulletColorIndex >= bulletColors.length) {
+      this.bulletColorIndex = bulletColors.length - 1;
+    }
+    // console.log(bulletColors[this.bulletColorIndex]);
+    this.timeout = window.setTimeout((): void => {
+      this.ageColor();
+    }, this.ageRate);
   }
 
   public redraw(): void {
     this.bullet.clear();
-    this.bullet.beginFill(0x000000);
+    this.bullet.beginFill(bulletColors[this.bulletColorIndex]);
     this.bullet.drawCircle(this.x, this.y, 1.5);
     this.bullet.endFill();
     // something
   }
 
   public destroy(): void {
-    //
+    window.clearTimeout(this.timeout);
   }
 }
