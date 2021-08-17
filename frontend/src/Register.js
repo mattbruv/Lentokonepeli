3df89473667212f9164831849eecc6716ac479bd
Matt P <5638426+mattbruv@users.noreply.github.com>
2021-08-17 17:39:51 -0400

    register input logic

--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -6,6 +6,7 @@ export class Register extends React.Component {
         super(props);
         this.state = {
             OK: false,
+            errors: [],
             username: "",
             pass1: "",
             pass2: "",
@@ -13,7 +14,8 @@ export class Register extends React.Component {
     }
 
     isValidUsername(name) {
-        return name.length >= 3 && name.length <= 20;
+        const re = new RegExp("^[a-zA-Z0-9-_]{3,20}$");
+        return name.match(re) !== null;
     }
 
     isValidPass(pass) {
@@ -22,24 +24,28 @@ export class Register extends React.Component {
 
     validateInput() {
         let OK = true;
+        const errors = [];
 
         if (!this.isValidUsername(this.state.username)) {
+            errors.push("Username must be between 3 and 20 characters and may only contain letters, numbers, underscores, and hypens");
             OK = false;
         }
 
         if (!this.isValidPass(this.state.pass1)) {
+            errors.push("Password must be at least 3 digits");
             OK = false;
         }
 
         if (this.state.pass1 !== this.state.pass2) {
+            errors.push("Passwords must be equal");
             OK = false;
         }
 
-        this.setState({ OK });
+        this.setState({ OK, errors });
     }
 
     handleChange(prop, event) {
-        console.log(prop, event.target.value);
+        // console.log(prop, event.target.value);
         this.setState({ [prop]: event.target.value }, () => {
             this.validateInput();
         });
@@ -53,6 +59,9 @@ export class Register extends React.Component {
                 <div>Password: <input onChange={(e) => this.handleChange("pass1", e)} type="password" /></div>
                 <div>Repeat Password: <input onChange={(e) => this.handleChange("pass2", e)} type="password" /></div>
                 <div><input disabled={!this.state.OK} type="button" value="Register" /></div>
+                {this.state.errors.map(e => {
+                    return <div className="error" key={e}>{e}</div>
+                })}
             </div>
         );
     }
