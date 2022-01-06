6405bf037811d838fb2d5627b4ff2ddebfaec17c
Matt <5638426+mattbruv@users.noreply.github.com>
2022-01-05 21:47:35 -0500

    get project building on desktop

--- client/src/game/entities/ground.ts
+++ client/src/game/entities/ground.ts
@@ -4,37 +4,35 @@ import { Entity } from "../entity";
 import { getTexture } from "../resources";
 
 export interface GroundProps {
-    x?: number;
-    y?: number;
-    width?: number;
-    type?: number;
+  x?: number;
+  y?: number;
+  width?: number;
+  type?: number;
 }
 
 export class Ground implements Entity {
+  type = EntityType.GROUND;
 
-    type = EntityType.GROUND;
+  sprite = new PIXI.TilingSprite(getTexture("ground1.gif")!);
 
-    sprite = new PIXI.TilingSprite(getTexture("ground1.gif")!);
+  constructor() {
+    this.sprite.height = this.sprite.texture.height;
+  }
 
-    constructor() {
-        this.sprite.height = this.sprite.texture.height;
+  destroy() {}
+
+  update(props: GroundProps) {
+    if (props.x !== undefined) {
+      this.sprite.position.x = props.x;
     }
-
-    destroy() { }
-
-    update(props: GroundProps) {
-        if (props.x) {
-            this.sprite.position.x = props.x;
-        }
-        if (props.y) {
-            this.sprite.position.y = props.y;
-        }
-        if (props.width) {
-            this.sprite.width = props.width;
-        }
-        if (props.type) {
-            console.log("change ground type!");
-        }
+    if (props.y !== undefined) {
+      this.sprite.position.y = props.y;
     }
-
+    if (props.width !== undefined) {
+      this.sprite.width = props.width;
+    }
+    if (props.type !== undefined) {
+      console.log("change ground type!");
+    }
+  }
 }
