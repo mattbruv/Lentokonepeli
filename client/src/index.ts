75aafe35a79c9f15fb4403c09faccaa524340ac6
Matt <5638426+mattbruv@users.noreply.github.com>
2022-01-06 20:33:23 -0500

    start static ui

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,2 +1,1 @@
-export * from "./network/api/APIPacketOut";
 export * from "./network/socket";
---
+++ ui/src/main.ts
@@ -0,0 +1,8 @@
+import "../css/style.css";
+
+const app = document.querySelector<HTMLDivElement>("#app")!;
+
+app.innerHTML = `
+  <h1>Hello Vite!</h1>
+  hi
+`;
