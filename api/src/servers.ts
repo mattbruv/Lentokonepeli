0f542e71b37d88e814f571d88e522efa07b97697
Matt <5638426+mattbruv@users.noreply.github.com>
2022-01-06 19:48:15 -0500

    remove api

--- api/src/servers.ts
+++
@@ -1,8 +0,0 @@
-import { ServerInfo } from "lento-client";
-
-export function getServers(): ServerInfo[] {
-    return [{
-        "region": "US East",
-        "url": "ws://localhost:6969",
-    }];
-}
