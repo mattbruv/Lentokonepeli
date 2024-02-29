c6d4982db68ee4406a21e0350a45ea0bfce48be4
Matt P <mattp3259@gmail.com>
2024-02-28 19:17:37 -0600

    field names example

--- networkable-derive/src/lib.rs
+++ networkable-derive/src/lib.rs
@@ -1,8 +1,9 @@
+use core::panic;
 use std::process::id;
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::DeriveInput;
+use syn::{DeriveInput, Ident};
 
 #[proc_macro_derive(Networkable)]
 pub fn networkable_derive_macro(item: TokenStream) -> TokenStream {
@@ -17,11 +18,31 @@ fn impl_networkable_trait(ast: DeriveInput) -> TokenStream {
     let ident = ast.ident;
     let ident_str = ident.to_string();
 
+    let fields: Vec<Ident> = match ast.data {
+        syn::Data::Struct(s) => s.fields.into_iter().filter_map(|f| f.ident).collect(),
+        syn::Data::Enum(_) => panic!("No enums"),
+        syn::Data::Union(_) => panic!("oh shit"),
+    };
+
+    let strs: Vec<String> = fields.iter().map(|i| i.to_string()).collect();
+
     // generate impl
     quote! {
+
+        #[derive(Debug)]
+        pub enum MacroEnum {
+            Bar,
+            Baz,
+            Matt
+        }
+
         impl Networkable for #ident {
             fn name(&self) -> &'static str {
                 #ident_str
+            }
+
+            fn field_names(&self) -> Vec<&'static str> {
+                vec![#(#strs),*]
             }
         }
     }
