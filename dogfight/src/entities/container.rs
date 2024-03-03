f4e0364a49526b86b55e5e836a223a7ea7f82b63
Matt P <mattp3259@gmail.com>
2024-03-03 13:29:58 -0600

    hleper fns

--- dogfight/src/entities/container.rs
+++ dogfight/src/entities/container.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, ops::Index};
 
 use super::EntityId;
 
@@ -10,8 +10,16 @@ pub struct EntityContainer<T> {
 impl<T> EntityContainer<T> {
     pub fn new() -> EntityContainer<T> {
         EntityContainer {
-            ids: (0..(2 as u16).pow(10)).rev().collect(),
+            ids: (0..(2 as EntityId).pow(10)).rev().collect(),
             map: HashMap::new(),
         }
+    }
+
+    pub fn get_mut(&mut self, id: EntityId) -> Option<&mut T> {
+        self.map.get_mut(&id)
+    }
+
+    pub fn get(&self, id: EntityId) -> Option<&T> {
+        self.map.get(&id)
     }
 }
