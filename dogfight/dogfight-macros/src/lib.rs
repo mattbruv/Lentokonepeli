6c988e18b0c730a4b5893155c3f36658236c0d05
Matt P <mattp3259@gmail.com>
2024-03-03 14:30:43 -0600

    don't conflict with rust's test macro

--- dogfight/dogfight-macros/src/lib.rs
+++ dogfight/dogfight-macros/src/lib.rs
@@ -1,5 +1,6 @@
-extern crate proc_macro;
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, Attribute, DeriveInput};
 
 #[proc_macro_attribute]
 pub fn network(_attr: TokenStream, item: TokenStream) -> TokenStream {
@@ -7,8 +8,16 @@ pub fn network(_attr: TokenStream, item: TokenStream) -> TokenStream {
 }
 
 #[proc_macro_attribute]
-pub fn show_streams(attr: TokenStream, item: TokenStream) -> TokenStream {
-    println!("attr: \"{}\"", attr.to_string());
-    println!("item: \"{}\"", item.to_string());
-    item
+pub fn lento_test(attr: TokenStream, item: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(item as DeriveInput);
+    // let attributes = parse_macro_input!(attr as Attribute);
+    let x = input.ident.to_owned();
+
+    let new_attrs = quote! {
+        #[derive(TS)]
+        #[ts(export)]
+        #input
+    };
+
+    TokenStream::from(new_attrs)
 }
