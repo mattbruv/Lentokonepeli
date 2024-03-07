07cca03d38ab9b38e6fa39f78f2fa03b1e8e328f
Matt P <mattp3259@gmail.com>
2024-03-07 17:16:24 -0600

    do it this way

--- dogfight/src/network/mod.rs
+++ dogfight/src/network/mod.rs
@@ -1,2 +1,22 @@
+use crate::entities::{man::*, EntityId};
+
 pub mod entity_props;
 pub mod update;
+
+trait NetworkedEntity {
+    fn get_full_state(&self) -> EntityState;
+    fn get_changed_state(&self) -> EntityState;
+}
+
+pub enum EntityState {
+    Full(EntityId, FullState),
+    Changed(EntityId, ChangedState),
+}
+
+pub enum ChangedState {
+    Man(EntityId, ChangedManState),
+}
+
+pub enum FullState {
+    Man(EntityId, FullManState),
+}
--- dogfight/src/network/update.rs
+++
@@ -1,8 +0,0 @@
-use crate::entities::{man::*, EntityId};
-use ts_rs::TS;
-
-pub enum FullState {
-    Man(EntityId, ManState),
-}
-
-pub enum PartialState {}
