36403f07b16749f89b50584438e4f94a73feee6c
Matt P <mattp3259@gmail.com>
2024-04-01 17:45:35 -0500

    current

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,26 +1,7 @@
-use dogfight::network::{state_to_bytes, state_to_json};
-use dogfight::world::World;
-use serde::Serialize;
-use std::time::Instant;
-
-use bincode::{config, Decode, Encode};
-use ts_rs::TS;
-
-#[derive(Encode, Decode, Debug, Serialize, TS)]
-#[ts(export)]
-pub struct Test {
-    x: i16,
-    y: i16,
-}
+use std::num;
 
 fn main() {
-    let world = World::new();
-
-    let config = config::standard();
-
-    let json = state_to_json(world.get_state());
-    let bin = state_to_bytes(world.get_state());
-
-    println!("{}", json);
-    println!("{:?}", bin);
+    println!("{}", res);
 }
+
+// se me detiene el corazon
