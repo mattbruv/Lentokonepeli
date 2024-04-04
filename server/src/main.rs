c5882572717a9451144e09eb635ace2b0c5ea99a
Matt Pitts <matt.pitts@nuqleous.com>
2024-04-04 15:20:50 -0600

    speed test

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,17 @@
-use std::num;
+use std::time::Instant;
+
+use dogfight::world::World;
 
 fn main() {
-    println!("{}", res);
+    let mut world: World = World::new();
+
+    let time = Instant::now();
+
+    world.get_full_state();
+
+    let elapsed = time.elapsed();
+
+    println!("{:?} elapsed", elapsed)
 }
 
 // se me detiene el corazon
