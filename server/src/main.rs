c1a313d7d7752688186650f39dd3efb0d395637c
Matt Pitts <matt.pitts@nuqleous.com>
2024-04-11 23:55:58 -0600

    debug parsing code

--- server/src/main.rs
+++ server/src/main.rs
@@ -7,7 +7,18 @@ fn main() {
     let world: World = World::new();
 
     let state = world.get_full_state();
-    let bytes: Vec<Vec<u8>> = state.iter().map(|x| x.to_bytes()).collect();
+    let bytes: Vec<u8> = state.iter().flat_map(|x| x.to_bytes()).collect();
 
+    println!("{:?}", state);
     println!("{:?}", bytes);
+
+    let mut bytes_copy = bytes.as_slice();
+    let mut parsed: Vec<EntityChange> = vec![];
+    println!("{:?}", bytes_copy);
+
+    while bytes_copy.len() > 0 {
+        let (new_slice, data) = EntityChange::from_bytes(&bytes_copy);
+        parsed.push(data);
+        bytes_copy = new_slice;
+    }
 }
