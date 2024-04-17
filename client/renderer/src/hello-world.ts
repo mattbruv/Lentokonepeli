3317dfe1fa62b50784bbaedff09fd19a4d2f1f16
Matt P <mattp3259@gmail.com>
2024-04-16 20:39:04 -0600

    type safe data deserializing

--- client/renderer/src/hello-world.ts
+++ client/renderer/src/hello-world.ts
@@ -1,14 +1,23 @@
 import { EntityChange } from "./bindings/EntityChange";
-import { EntityType } from "./bindings/EntityType";
-import { ManProperties } from "./bindings/ManProperties";
 
 export function sayHello() {
   console.log("Hey");
 }
 
-export function sayGoodbye(changes: EntityChange[]): ManProperties | null {
-  if (changes.length > 0) {
-    const first = changes[0];
+export function sayGoodbye(changes: EntityChange[]): void {
+  for (const change of changes) {
+    switch (change.update.type) {
+      case "Properties": {
+        const update = change.update.data;
+        if (update.type === "Man") {
+          console.log(update.props);
+        }
+
+        break;
+      }
+      case "Deleted": {
+        break;
+      }
+    }
   }
-  return null;
 }
