75b707f52d1b20817aa83dc06eb3f6db27a5527f
Matt P <mattp3259@gmail.com>
2024-04-16 23:39:24 -0600

    laod levels

--- dogfight/src/entities/background_item.rs
+++ dogfight/src/entities/background_item.rs
@@ -2,19 +2,21 @@ use dogfight_macros::Networked;
 
 use crate::network::{property::*, EntityProperties, NetworkedEntity};
 
-use super::types::{BackgroundItemType, Team};
+use super::types::{BackgroundItemType, Facing};
 
 #[derive(Networked)]
 pub struct BackgroundItem {
     bg_item_type: Property<BackgroundItemType>,
+    facing: Property<Facing>,
     client_x: Property<i16>,
     client_y: Property<i16>,
 }
 
 impl BackgroundItem {
-    pub fn new(bg_item_type: BackgroundItemType, x: i16, y: i16) -> Self {
+    pub fn new(bg_item_type: BackgroundItemType, facing: Facing, x: i16, y: i16) -> Self {
         Self {
             bg_item_type: Property::new(bg_item_type),
+            facing: Property::new(facing),
             client_x: Property::new(x),
             client_y: Property::new(y),
         }
