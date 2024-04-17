1ab96004084b67c6485134b34bbe3b8a8feb97be
Matt Pitts <matt.pitts@nuqleous.com>
2024-04-16 23:18:40 -0600

    level load test

--- server/src/main.rs
+++ server/src/main.rs
@@ -5,31 +5,7 @@ use dogfight::{
 
 fn main() {
     let mut world: World = World::new();
+    let levels: Vec<&str> = include_str!("../levels.txt").split("\n\n").collect();
 
-    let state = world.get_full_state();
-    let bytes: Vec<u8> = state.iter().flat_map(|x| x.to_bytes()).collect();
-
-    println!("{:?}", state);
-    println!("{:?}", bytes);
-
-    let mut bytes_copy = bytes.as_slice();
-    let mut parsed: Vec<EntityChange> = vec![];
-
-    while bytes_copy.len() > 0 {
-        let (new_slice, data) = EntityChange::from_bytes(&bytes_copy);
-        parsed.push(data);
-        bytes_copy = new_slice;
-    }
-
-    let a = format!("{:?}", state);
-    let b = format!("{:?}", parsed);
-
-    println!("{:?}", a == b);
-
-    let test = Some("optional string test".to_string());
-    let bytes = test.to_bytes();
-    println!("{:?}", bytes);
-    let read = Option::<String>::from_bytes(&bytes);
-    println!("{:?}", read.1);
-    println!("{:?}", read.0);
+    world.load_level(levels[0]);
 }
