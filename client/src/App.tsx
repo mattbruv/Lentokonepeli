2e66e7cd80d1d91fdee3f73ff99b589d46e98f73
Matt P <mattp3259@gmail.com>
2024-04-18 17:16:42 -0600

    retardation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,10 @@
 import { useContext, useState } from "react";
 import "./App.css";
 import { DogfightContext } from "./DogfightContext";
+import { Game } from "./Game";
 
 function App() {
   const dogfight = useContext(DogfightContext);
-
-  const x = dogfight.game.get_changed_state_binary();
-  console.log(x);
 
   // TODO: read string from client and interpret it as a level,
   // render internal state to screen
@@ -14,9 +12,7 @@ function App() {
   return (
     <>
       <h1>Lento</h1>
-      <div>
-        <p>test</p>
-      </div>
+      <Game />
     </>
   );
 }
