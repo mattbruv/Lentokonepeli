66d395c0d6f17623c71d8ebd5ee1749ef6a97e31
Matt Pitts <matt.pitts@nuqleous.com>
2024-04-18 16:23:46 -0600

    add renderer

---
+++ client/src/client/constants.ts
@@ -0,0 +1,22 @@
+export const enum DrawLayer {
+  LAYER_17,
+  LAYER_16,
+  LAYER_15,
+  LAYER_14,
+  LAYER_13,
+  LAYER_12,
+  LAYER_11,
+  LAYER_10,
+  LAYER_09,
+  LAYER_08,
+  LAYER_07,
+  LAYER_11_LAYER_13,
+  LAYER_10_LAYER_12,
+}
+
+export const enum WaterColor {
+  NORMAL = 3051728,
+  DESERT = 2344139,
+}
+
+export const SKY_COLOR = 14540287;
