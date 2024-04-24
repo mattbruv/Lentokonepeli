339923f9e0b0fb2ae3566939187672486f5b6c8a
Matt Pitts <matt.pitts@nuqleous.com>
2024-04-23 18:43:53 -0600

    aisdjfoiajsdf

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,19 @@
-import { useContext, useState } from "react";
+import { Box } from "@mantine/core";
 import "./App.css";
-import { DogfightContext } from "./DogfightContext";
 import { Game } from "./Game";
-import levels from "./assets/levels.json";
 
 function App() {
   return (
-    <>
-      <h1>Lento</h1>
+    <Box
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
       <Game />
-      {/*
-      {Object.entries(levels).map((x) => {
-        return (
-          <>
-            <p key={x[0]}>
-              <code style={{ whiteSpace: "pre-wrap" }}>{x[1]}</code>
-            </p>
-          </>
-        );
-      })}
-      */}
-    </>
+    </Box>
   );
 }
 
