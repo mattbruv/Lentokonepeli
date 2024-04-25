b8ead59f6ae4f05ab05eb2cf4a2047dfa06e308c
Matt Pitts <matt.pitts@nuqleous.com>
2024-04-24 23:45:22 -0600

    start talking to the game

---
+++ dogfight/src/tick_input.rs
@@ -0,0 +1,57 @@
+use serde::Serialize;
+use ts_rs::TS;
+
+use crate::{entities::types::Team, network::encoding::NetworkedBytes};
+
+/*
+    Represents anything that the game world needs to send to the
+    client after every game tick.
+*/
+#[derive(Serialize, Debug, TS)]
+#[ts(export)]
+#[serde(tag = "type", content = "data")]
+pub enum TickInput {
+    AddPlayer { name: String },
+    RemovePlayer { name: String },
+    SetPlayerTeam(Team),
+}
+
+impl NetworkedBytes for TickInput {
+    fn to_bytes(&self) -> Vec<u8> {
+        let mut bytes: Vec<u8> = vec![];
+
+        match &self {
+            GameEvent::EntityChanges(changes) => {
+                bytes.push(0);
+                let len = changes.len();
+                assert!(len < 256, "Change count exceeds byte size");
+                bytes.push(len as u8);
+                bytes.extend(entity_changes_to_binary(changes));
+            }
+        }
+
+        bytes
+    }
+
+    fn from_bytes(bytes: &[u8]) -> (&[u8], Self) {
+        let (mut slice, variant) = u8::from_bytes(bytes);
+
+        match variant {
+            0 => {
+                // parse entity changes
+                let (bytes, change_length) = u8::from_bytes(slice);
+                slice = &bytes;
+                let mut changes = vec![];
+
+                for _ in 0..change_length {
+                    let (bytes, update) = EntityChange::from_bytes(slice);
+                    slice = &bytes;
+                    changes.push(update);
+                }
+
+                (slice, GameEvent::EntityChanges(changes))
+            }
+            _ => panic!("Unrecognized enum variant in Event: {}", variant),
+        }
+    }
+}
