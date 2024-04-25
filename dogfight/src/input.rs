098aa14ccab1559b423de922fbd1f8eb8f7fba98
Matt P <mattp3259@gmail.com>
2024-04-25 17:30:39 -0600

    work on this

--- dogfight/src/input.rs
+++ dogfight/src/input.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 use ts_rs::TS;
 
-use crate::entities::types::Team;
+use crate::entities::{plane::PlaneType, types::Team, EntityId};
 
 /*
     Represents any type of input to affect the game world during a tick
@@ -12,10 +12,25 @@ use crate::entities::types::Team;
 pub enum GameInput {
     AddPlayer { name: String },
     RemovePlayer { name: String },
-    PlayerChooseTeam { name: String, team: Team },
-    PlayerChooseRunway { name: String, runway_id: u16 },
+    PlayerChooseTeam(TeamSelection),
+    PlayerChooseRunway(RunwaySelection),
 }
 
 pub fn game_input_from_string(input: String) -> Vec<GameInput> {
     serde_json::from_str(&input).unwrap()
+}
+
+#[derive(Serialize, Deserialize, Debug, TS)]
+#[ts(export)]
+pub struct RunwaySelection {
+    player_name: String,
+    runway_id: EntityId,
+    plane_type: PlaneType,
+}
+
+#[derive(Serialize, Deserialize, Debug, TS)]
+#[ts(export)]
+pub struct TeamSelection {
+    player_name: String,
+    team: Option<Team>, // None team selection means random
 }
