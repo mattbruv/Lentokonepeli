45c392f8093ef492b01875693680aa4c7b022d05
Matt Pitts <matt.pitts@nuqleous.com>
2024-04-24 22:18:07 -0600

    mor echanges

--- dogfight/dogfight-web/src/lib.rs
+++ dogfight/dogfight-web/src/lib.rs
@@ -1,12 +1,19 @@
 mod utils;
 
-use dogfight::network::{entity_changes_to_binary, entity_changes_to_json};
-use dogfight::world::World;
+// use dogfight::network::{entity_changes_to_binary, entity_changes_to_json};
+use dogfight::{
+    event::GameEvent,
+    network::{
+        encoding::NetworkedBytes, game_events_from_bytes, game_events_to_binary,
+        game_events_to_json,
+    },
+    world::World,
+};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 extern "C" {
-    fn alert(s: &str);
+    fn alert();
 }
 
 #[wasm_bindgen]
@@ -22,13 +29,15 @@ impl DogfightWeb {
         }
     }
 
-    pub fn get_full_state(&self) -> String {
-        entity_changes_to_json(self.world.get_full_state())
-    }
+    /*
+        pub fn get_full_state(&self) -> String {
+            entity_changes_to_json(self.world.get_full_state())
+        }
 
-    pub fn get_full_state_binary(&self) -> Vec<u8> {
-        entity_changes_to_binary(&self.world.get_full_state())
-    }
+        pub fn get_full_state_binary(&self) -> Vec<u8> {
+            entity_changes_to_binary(&self.world.get_full_state())
+        }
+    */
 
     pub fn load_level(&mut self, world: String) {
         self.world.load_level(&world);
@@ -38,7 +47,10 @@ impl DogfightWeb {
         self.world.init();
     }
 
-    pub fn tick(&mut self) -> () {
+    pub fn tick(&mut self) -> String {
         let events = self.world.tick();
+        let bin = game_events_to_binary(events);
+        let parsed = game_events_from_bytes(bin);
+        game_events_to_json(parsed)
     }
 }
