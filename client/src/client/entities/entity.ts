c1b39ce1617d929c09fe7a74e21249e87abcff19
Matt Pitts <matt.pitts@nuqleous.com>
2024-04-26 23:51:35 -0600

    is this working:?

--- client/src/client/entities/entity.ts
+++ client/src/client/entities/entity.ts
@@ -2,18 +2,21 @@ import * as PIXI from "pixi.js";
 
 export type NoUndefined<T> = T extends undefined ? never : T;
 
-export type EntityUpdateCallbacks<Source> = {
+export type EntityUpdateCallbacks<Source extends object> = {
   [Property in keyof Source]-?: (value: NoUndefined<Source[Property]>) => void;
 };
 
-export type Entity<Props> = {
+export type Entity<Props extends object> = {
   props: Props;
   updateCallbacks: () => EntityUpdateCallbacks<Props>;
   getContainer: () => PIXI.Container;
   destroy: () => void;
 };
 
-export function updateProps<Props>(entity: Entity<Props>, props: Props) {
+export function updateProps<Props extends Object>(
+  entity: Entity<Props>,
+  props: Props
+) {
   console.log("BEFORE :", JSON.stringify(entity.props));
 
   entity.props = {
@@ -25,10 +28,11 @@ export function updateProps<Props>(entity: Entity<Props>, props: Props) {
 
   console.log(props);
   for (const key in props) {
-    if (props.hasOwnProperty(key)) console.log(key);
-    const propUpdateCallback = callbacks[key];
-    const value = props[key];
-    propUpdateCallback(value as NoUndefined<typeof value>);
+    if (props.hasOwnProperty(key)) {
+      const propUpdateCallback = callbacks[key];
+      const value = props[key];
+      propUpdateCallback(value as NoUndefined<typeof value>);
+    }
   }
   console.log("AFTER :", JSON.stringify(entity.props));
 }
