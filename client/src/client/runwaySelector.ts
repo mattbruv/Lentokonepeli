62d2384bfcc0ce55749d60fc43393d818e62ff29
Matt P <mattp3259@gmail.com>
2024-04-27 22:16:57 -0600

    start this

--- client/src/client/runwaySelector.ts
+++ client/src/client/runwaySelector.ts
@@ -1,12 +1,23 @@
 import * as PIXI from "pixi.js";
 import { Textures } from "./textures";
 import { GameClientCallbacks } from "./DogfightClient";
+import { PlaneType } from "dogfight-types/PlaneType";
+import { Team } from "dogfight-types/Team";
+
+type PlaneData = {
+  plane_type: PlaneType;
+  texture: PIXI.Texture;
+};
+
+type PlaneMap = Record<Team, PlaneData[]>;
 
 export class RunwaySelector {
   public container: PIXI.Container;
 
   public infoBox: PIXI.Sprite;
   public planeImage: PIXI.Sprite;
+
+  public planeMap: PlaneMap;
 
   constructor() {
     this.container = new PIXI.Container();
@@ -17,9 +28,18 @@ export class RunwaySelector {
     this.container.addChild(this.infoBox);
     this.container.addChild(this.planeImage);
     this.container.visible = false;
+
+    this.planeImage.position.set(10);
+
+    this.planeMap = {
+      Allies: [],
+      Centrals: [],
+    };
   }
 
   public init(callbacks: GameClientCallbacks) {
     this.infoBox.texture = Textures["selectionScreen.gif"];
+
+    this.planeImage.texture = Textures["pic_plane4.gif"];
   }
 }
