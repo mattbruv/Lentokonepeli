a5dc12fb313a51dd024551653ca9d394a6b0ba64
Matt Pitts <matt.pitts@nuqleous.com>
2024-04-28 11:41:12 -0600

    comment this out

--- dogfight/dogfight-web/src/utils.rs
+++ dogfight/dogfight-web/src/utils.rs
@@ -1,3 +1,4 @@
+/*
 pub fn set_panic_hook() {
     // When the `console_error_panic_hook` feature is enabled, we can call the
     // `set_panic_hook` function at least once during initialization, and then
@@ -8,3 +9,4 @@ pub fn set_panic_hook() {
     #[cfg(feature = "console_error_panic_hook")]
     console_error_panic_hook::set_once();
 }
+*/
