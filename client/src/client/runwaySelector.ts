73b8d654bb159a75738c2d6fa70abe3fb6a8764f
Matt Pitts <matt.pitts@nuqleous.com>
2024-04-27 22:24:15 -0600

    add plane images

--- client/src/client/runwaySelector.ts
+++ client/src/client/runwaySelector.ts
@@ -40,6 +40,36 @@ export class RunwaySelector {
   public init(callbacks: GameClientCallbacks) {
     this.infoBox.texture = Textures["selectionScreen.gif"];
 
+    this.planeMap.Centrals.push({
+      plane_type: "Albatros",
+      texture: Textures["pic_plane4.gif"],
+    });
+
+    this.planeMap.Centrals.push({
+      plane_type: "Fokker",
+      texture: Textures["pic_plane6.gif"],
+    });
+
+    this.planeMap.Centrals.push({
+      plane_type: "Junkers",
+      texture: Textures["pic_plane5.gif"],
+    });
+
+    this.planeMap.Allies.push({
+      plane_type: "Bristol",
+      texture: Textures["pic_plane7.gif"],
+    });
+
+    this.planeMap.Allies.push({
+      plane_type: "Sopwith",
+      texture: Textures["pic_plane9.gif"],
+    });
+
+    this.planeMap.Allies.push({
+      plane_type: "Salmson",
+      texture: Textures["pic_plane8.gif"],
+    });
+
     this.planeImage.texture = Textures["pic_plane4.gif"];
   }
 }
