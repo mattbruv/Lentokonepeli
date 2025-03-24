6440f304a3d7c9e2789756a87cfd78dd31087b28
Arttu Pennanen <39066923+pennane@users.noreply.github.com>
2025-03-24 01:46:11 +0100

    Correct draw z index for water & back of runway (#63)

--- client/src/client/constants.ts
+++ client/src/client/constants.ts
@@ -3,26 +3,33 @@ import { Terrain } from "dogfight-types/Terrain";
 export const VIEW_WIDTH = 740;
 export const VIEW_HEIGHT = 660;
 // 510 hardcoded bg, plus 150 UI height, 660 total height?
+/**
+ * Entities defined last are drawn in front of entities defined before them
+ */
+const ENTITY_Z_INDEX_ORDER = [
+    "Sky",
+    "Hill",
+    "Flag",
+    "ControlTower",
+    "Ground",
+    "RunwayBack",
+    "LightSmoke",
+    "Plane",
+    "Water",
+    "DarkSmoke",
+    "Bunker",
+    "Runway",
+    "Man",
+    "Explosion",
+    "Bomb",
+    "Bullet",
+    "Player",
+] as const;
 
-export enum DrawLayer {
-    Sky = -999,
-    Hill = -120,
-    Water = -100,
-    Flag = -70,
-    ControlTower = -60,
-    Ground = -50,
-    RunwayBack = -10,
-    LightSmoke = -7,
-    Plane = -5,
-    DarkSmoke = -3,
-    Bunker = -1,
-    Runway = 0,
-    Man = 5,
-    Explosion = 50,
-    Bomb = 60,
-    Bullet = 69,
-    Player = 100,
-}
+export const DrawLayer = Object.fromEntries(ENTITY_Z_INDEX_ORDER.map((entity, i) => [entity, i])) as Record<
+    (typeof ENTITY_Z_INDEX_ORDER)[number],
+    number
+>;
 
 export const enum WaterColor {
     NORMAL = 3051728,
