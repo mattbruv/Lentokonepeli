1d3d7dc16027751733cb8f71183d12d48b24ae34
Arttu Pennanen <arttu@pennanen.org>
2025-03-24 14:57:41 +0100

    Add motorsound

--- client/src/client/soundManager.ts
+++ client/src/client/soundManager.ts
@@ -1,18 +1,34 @@
+import { Howl } from "howler";
+
 class SoundManager {
-    private isMuted: boolean = false;
+    private activeEngines: number = 0;
+    private engineAudio = new Howl({
+        src: "audio/motor_paulstretch.wav",
+        loop: true,
+    });
 
     public setIsMuted(state: boolean) {
-        console.log("Muted:", state);
-        this.isMuted = state;
-    }
-
-    public soundEnabled(): boolean {
-        return !this.isMuted;
+        Howler.mute(state);
     }
 
     public playSound(sound: Howl) {
-        if (!this.isMuted) {
-            sound.play();
+        sound.play();
+    }
+
+    /**
+     * Play motor sound if at least 1 plane with motor on
+     */
+    public handlePlayMotorSound(motorOn: boolean) {
+        const engineSoundWasPlaying = this.activeEngines > 0;
+
+        this.activeEngines = motorOn ? this.activeEngines + 1 : Math.max(this.activeEngines - 1, 0);
+
+        const engineSoundIsPlaying = this.activeEngines > 0;
+
+        if (!engineSoundWasPlaying && engineSoundIsPlaying) {
+            this.playSound(this.engineAudio);
+        } else if (engineSoundWasPlaying && !engineSoundIsPlaying) {
+            this.engineAudio.stop();
         }
     }
 }
